b98f315f8a7ab2afb8190b106d9cfccef38fa6d1
Rekha Kumari <rekha@Rekhas-MacBook-Pro.local>
2021-08-17 14:14:04 +0530

    added addMovieInfo function

--- src/SeatBooking.ts
+++ src/SeatBooking.ts
@@ -1,7 +1,7 @@
 import { ShowTimings, BookedSeats } from '../src/types/timings'
 import { AvailableSeats, date } from '../src/types/day'
 import { totalSeats} from './constant/constant'
-export const bookMyShow = (timeSlot: ShowTimings, state:AvailableSeats, currentDate:date) => {
+export const getShowInfo = (timeSlot: ShowTimings, state:AvailableSeats, currentDate:date) => {
     if(state[currentDate][timeSlot]["seat"]< totalSeats){
         const updatedState = {
             ...state,
@@ -12,4 +12,21 @@ export const bookMyShow = (timeSlot: ShowTimings, state:AvailableSeats, currentD
     }else{
         return "No seats are available"
     }
+}
+
+export const addMovieInformation = (timeSlot:ShowTimings, state:AvailableSeats, movieName:string, currentDate:date)=> {
+    const {movie, seat} = state[currentDate][timeSlot]
+    if(movie !== null && seat===0){
+        return `Movie is already assigned to this timeslot`
+    }
+    else{
+        const updatedState = {
+            ...state,
+            movie: movieName,
+            time:timeSlot,
+            day:currentDate
+        }
+        return updatedState
+    }
+    
 }
--- test/src/seatBooking.test.ts
+++ test/src/seatBooking.test.ts
@@ -1,4 +1,4 @@
-import { bookMyShow } from '../../src/SeatBooking'
+import { getShowInfo, addMovieInformation } from '../../src/SeatBooking'
 import { AvailableSeats } from '../../src/types/day'
 describe("BookMyShow Test", () => {
     const initialState: AvailableSeats={
@@ -9,15 +9,15 @@ describe("BookMyShow Test", () => {
             },
             AFTERNOON: {
                 seat:48,
-                movie:"Movie2"
+                movie:"Movie1"
             },
             EVENING: {
                 seat: 3,
-                movie: "Movie3"
+                movie: "Movie1"
             },
             NIGHT: {
                 seat: 79,
-                movie:"Movie4"
+                movie:"Movie1"
             }
         },
         "2-08-21" : {
@@ -27,33 +27,33 @@ describe("BookMyShow Test", () => {
             },
             AFTERNOON: {
                 seat:47,
-                movie:"Movie2"
+                movie:"Movie1"
             },
             EVENING: {
                 seat: 2,
-                movie: "Movie3"
+                movie: "Movie1"
             },
             NIGHT: {
                 seat: 76,
-                movie:"Movie4"
+                movie:"Movie1"
             }
         },
         "3-08-21" : {
             MORNING: {
-                seat:2,
-                movie:"Movie1"
+                seat:0,
+                movie:null
             },
             AFTERNOON: {
                 seat:4,
-                movie:"Movie2"
+                movie:"Movie1"
             },
             EVENING: {
                 seat: 37,
-                movie: "Movie3"
+                movie: "Movie1"
             },
             NIGHT: {
                 seat: 7,
-                movie:"Movie4"
+                movie:"Movie1"
             }
         },
         "4-08-21" : {
@@ -63,26 +63,35 @@ describe("BookMyShow Test", () => {
             },
             AFTERNOON: {
                 seat:100,
-                movie:"Movie2"
+                movie:"Movie1"
             },
             EVENING: {
                 seat: 35,
-                movie: "Movie3"
+                movie: "Movie1"
             },
             NIGHT: {
-                seat: 71,
-                movie:"Movie4"
+                seat: 0,
+                movie:"Movie2"
             }
         }
     }
     it("should return seat numbers which is available", () => {
-        const availableSeat = bookMyShow("MORNING", initialState, "1-08-21")
+        const availableSeat = getShowInfo("MORNING", initialState, "1-08-21")
         expect(availableSeat).toEqual({...initialState, seat: 1, movie:"Movie1"})
     })
 
     it("should return error if seat number not available", () => {
-        const availableSeat = bookMyShow("AFTERNOON", initialState, "4-08-21")
+        const availableSeat = getShowInfo("AFTERNOON", initialState, "4-08-21")
         expect(availableSeat).toBe("No seats are available")
     })
 
+    it("should add the movie information", () => {
+        const setMovieInfo = addMovieInformation("MORNING", initialState, "Movie2", "3-08-21")
+        expect(setMovieInfo).toEqual({...initialState, time: "MORNING", movie: "Movie2", day:"3-08-21"})
+    })
+
+    it("should give error message if the movie is already added", () => {
+        const setMovieInfo = addMovieInformation("NIGHT", initialState, "Movie2", "4-08-21")
+        expect(setMovieInfo).toEqual("Movie is already assigned to this timeslot")
+    })
 })
